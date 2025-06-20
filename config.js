2e3b75ccdac8b7395f2c76f04319b59c7466b2e4
ブドウちゃん <budouchanbudouchan@gmail.com>
2025-06-20 16:39:55 +0900

    動画アップロードページ完成

---
+++ config.js
@@ -0,0 +1,9 @@
+// 設定値をここに置き換えてください
+const API_KEY = 'YOUR_API_KEY_HERE';
+const CLIENT_ID = 'YOUR_CLIENT_ID_HERE';
+
+// Google Drive APIの設定
+const SCOPES = ['https://www.googleapis.com/auth/drive.file'];
+
+// アップロード制限
+const MAX_FILE_SIZE = 2 * 1024 * 1024 * 1024; // 2GB
