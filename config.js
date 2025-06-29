b881addc5be43ab53ecd95746d2b498a72b4fc45
ブドウちゃん <budouchanbudouchan@gmail.com>
2025-06-29 12:04:11 +0900

    Add new test files and update config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // 設定値をここに置き換えてください
-const API_KEY = 'YOUR_API_KEY_HERE';
-const CLIENT_ID = 'YOUR_CLIENT_ID_HERE';
+const API_KEY = 'AIzaSyCH2IAbtvQI0ONUv43HvHvnCLiwiSi5iwg';
+const CLIENT_ID = '512329280979-o3762jl3r2taaa81k12ac8u00hv2qqhd.apps.googleusercontent.com';
 
 // Google Drive APIの設定
 const SCOPES = ['https://www.googleapis.com/auth/drive.file'];
