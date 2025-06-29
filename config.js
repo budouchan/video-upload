c3209df778a9f6d4c79a75abe283321fd470678a
ブドウちゃん <budouchanbudouchan@gmail.com>
2025-06-29 14:27:59 +0900

    feat: 環境変数を読み込むようにconfig.jsを更新

--- config.js
+++ config.js
@@ -1,9 +1,2 @@
-// 設定値をここに置き換えてください
-const API_KEY = 'AIzaSyCH2IAbtvQI0ONUv43HvHvnCLiwiSi5iwg';
-const CLIENT_ID = '512329280979-o3762jl3r2taaa81k12ac8u00hv2qqhd.apps.googleusercontent.com';
-
-// Google Drive APIの設定
-const SCOPES = ['https://www.googleapis.com/auth/drive.file'];
-
-// アップロード制限
-const MAX_FILE_SIZE = 2 * 1024 * 1024 * 1024; // 2GB
+const API_KEY = process.env.GOOGLE_API_KEY;
+const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
